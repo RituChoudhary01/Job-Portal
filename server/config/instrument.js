3950d9e79ab3eb26c04c8ae24b6572c04bccaf03
RituChoudhary01 <ritu9306541427@gmail.com>
2025-08-10 07:22:39 +0530

    Commit

--- server/config/instrument.js
+++ server/config/instrument.js
@@ -1,13 +1,13 @@
-// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
-import * as Sentry from "@sentry/node";
+// // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+// import * as Sentry from "@sentry/node";
 
-Sentry.init({
-  dsn: "https://1e3eded27f8cbfd259a24f95547b0967@o4509590624993280.ingest.us.sentry.io/4509590630498304",
-  integrations:[
-    Sentry.mongooseIntegration()
-  ],
+// Sentry.init({
+//   dsn: "https://1e3eded27f8cbfd259a24f95547b0967@o4509590624993280.ingest.us.sentry.io/4509590630498304",
+//   integrations:[
+//     Sentry.mongooseIntegration()
+//   ],
 
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-});
+//   // Setting this option to true will send default PII data to Sentry.
+//   // For example, automatic IP address collection on events
+//   sendDefaultPii: true,
+// });
