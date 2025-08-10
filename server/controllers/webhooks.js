7d229ba5d72040153c08a39748968fda1d315dc5
RituChoudhary01 <ritu9306541427@gmail.com>
2025-08-10 07:35:03 +0530

    Commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,54 +1,107 @@
-import { Webhook } from 'svix'
+// import { Webhook } from 'svix'
+// import User from '../models/User.js';
+// // API Controller Function to Manage Clerk User with database
+// export const clerkWebhooks = async(req,res)=>{
+//   try{
+//     // create a Svix instance with clerk webhook secret.
+//     const whook = new Webhook (process.env.CLERK_WEBHOOK_SECRET)
+//     // Verifying Headers
+//     await whook.verify(JSON.stringify(req.body),{
+//       'svix-id':req.headers['svix-id'],
+//       'svix-timestamp':req.headers['svix-timestamp'],
+//       'svix-signature':req.headers['svix-signature']
+//     })
+//     // Getting Data from request body
+//     const { data, type } = req.body
+//     // Switch Cases for different Events
+//     switch(type){
+//       case 'user.created':{
+//         const userData = {
+//           _id:data.id,
+//           email:data.email_addresses[0].email_address,
+//           name:data.first_name+" "+data.last_name,
+//           image:data.image_url,
+//           resume:''
+//         }
+//         await User.create(userData)
+//         res.json({})
+//         break;
+//       }
+//       case 'user.updated':{
+//         const userData = {
+//           email:data.email_addresses[0].email_address,
+//           name:data.first_name+" "+data.last_name,
+//           image:data.image_url,
+//         }
+//         await User.findByIdAndUpdate(data.id, userData)
+//         res.json({})
+//         break;
+//       }
+//       case 'user.deleted':{
+//        await User.findByIdAndDelete(data.id)
+//        res.json({success: true })
+//        break;
+//       }
+//       default:
+//         console.log(`Unhandled Clerk event type: ${type}`);
+//         res.status(200).end();
+//         break;
+//     }
+//   }catch(error){
+//    console.log(error.message);
+//    res.json({success:false, message:'Webhooks Error'})
+//   }
+// } 
+
+
+import { Webhook } from 'svix';
 import User from '../models/User.js';
-// API Controller Function to Manage Clerk User with database
-export const clerkWebhooks = async(req,res)=>{
-  try{
-    // create a Svix instance with clerk webhook secret.
-    const whook = new Webhook (process.env.CLERK_WEBHOOK_SECRET)
-    // Verifying Headers
-    await whook.verify(JSON.stringify(req.body),{
-      'svix-id':req.headers['svix-id'],
-      'svix-timestamp':req.headers['svix-timestamp'],
-      'svix-signature':req.headers['svix-signature']
-    })
-    // Getting Data from request body
-    const { data, type } = req.body
-    // Switch Cases for different Events
-    switch(type){
-      case 'user.created':{
+
+export const clerkWebhooks = async (req, res) => {
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    // Verify using rawBody (Buffer)
+    await whook.verify(req.rawBody, {
+      'svix-id': req.headers['svix-id'],
+      'svix-timestamp': req.headers['svix-timestamp'],
+      'svix-signature': req.headers['svix-signature'],
+    });
+
+    const { data, type } = req.body;
+
+    switch (type) {
+      case 'user.created': {
         const userData = {
-          _id:data.id,
-          email:data.email_addresses[0].email_address,
-          name:data.first_name+" "+data.last_name,
-          image:data.image_url,
-          resume:''
-        }
-        await User.create(userData)
-        res.json({})
-        break;
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          image: data.image_url,
+          resume: '',
+        };
+        await User.create(userData);
+        return res.json({});
       }
-      case 'user.updated':{
+      case 'user.updated': {
         const userData = {
-          email:data.email_addresses[0].email_address,
-          name:data.first_name+" "+data.last_name,
-          image:data.image_url,
-        }
-        await User.findByIdAndUpdate(data.id, userData)
-        res.json({})
-        break;
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          image: data.image_url,
+        };
+        await User.findByIdAndUpdate(data.id, userData);
+        return res.json({});
       }
-      case 'user.deleted':{
-       await User.findByIdAndDelete(data.id)
-       res.json({success: true })
-       break;
+      case 'user.deleted': {
+        await User.findByIdAndDelete(data.id);
+        return res.json({ success: true });
       }
       default:
         console.log(`Unhandled Clerk event type: ${type}`);
-        res.status(200).end();
-        break;
+        return res.status(200).end();
     }
-  }catch(error){
-   console.log(error.message);
-   res.json({success:false, message:'Webhooks Error'})
+  } catch (error) {
+    console.error('Webhook error:', error);
+    return res.status(400).json({ success: false, message: 'Webhooks Error' });
   }
-} 
+};
+
--- server/server.js
+++ server/server.js
@@ -1,3 +1,47 @@
+// import express from 'express'
+// import cors from 'cors'
+// import dotenv from 'dotenv';
+// import connectDB from './config/db.js'
+// import { clerkWebhooks } from './controllers/webhooks.js'
+// import companyRouters from './routes/companyRoutes.js'
+// import connectcloudinary from './config/cloudinary.js'
+// import jobRoutes from './routes/jobRoutes.js'
+// import userRoutes from './routes/userRoutes.js'
+// import { clerkMiddleware } from '@clerk/express'
+
+// // Initialize express
+// const app = express()
+
+// // Connect to database
+// dotenv.config();
+// await connectDB();
+// await connectcloudinary()
+
+// // Middlewares
+// app.use(
+//   express.json({
+//     verify: (req, res, buf) => {
+//       req.rawBody = buf; 
+//     }
+//   })
+// )
+// app.use(cors())
+// app.use(express.json())
+// app.use(clerkMiddleware())
+// // Routes
+// app.get('/', (req, res) => res.send('API Working for insideJobs'))
+// app.post('/webhooks', clerkWebhooks)
+// app.use('/api/company',companyRouters)
+// app.use('/api/jobs',jobRoutes)
+// app.use('/api/users',userRoutes)
+
+// // Port
+// const PORT = process.env.PORT || 5000;
+// Sentry.setupExpressErrorHandler(app);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv';
@@ -9,73 +53,31 @@ import jobRoutes from './routes/jobRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 
-// Initialize express
-const app = express()
-
-// Connect to database
 dotenv.config();
 await connectDB();
-await connectcloudinary()
+await connectcloudinary();
 
-// Middlewares
-// app.use(
-//   express.json({
-//     verify: (req, res, buf) => {
-//       req.rawBody = buf; 
-//     }
-//   })
-// )
-app.use(cors())
-app.use(express.json())
-app.use(clerkMiddleware())
-// Routes
-app.get('/', (req, res) => res.send('API Working for insideJobs'))
-app.post('/webhooks', clerkWebhooks)
-app.use('/api/company',companyRouters)
-app.use('/api/jobs',jobRoutes)
-app.use('/api/users',userRoutes)
+const app = express();
 
-// Port
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    }
+  })
+);
+
+app.use(cors());
+app.use(clerkMiddleware());
+
+app.get('/', (req, res) => res.send('API Working for insideJobs'));
+app.post('/webhooks', clerkWebhooks);
+
+app.use('/api/company', companyRouters);
+app.use('/api/jobs', jobRoutes);
+app.use('/api/users', userRoutes);
+
 const PORT = process.env.PORT || 5000;
-Sentry.setupExpressErrorHandler(app);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// // import './config/instrument.js';
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import connectDB from './config/db.js';
-
-// import { clerkWebhooks } from './controllers/webhooks.js';
-// import companyRouters from './routes/companyRoutes.js';
-// import connectcloudinary from './config/cloudinary.js';
-// import jobRoutes from './routes/jobRoutes.js';
-// import userRoutes from './routes/userRoutes.js';
-// import { clerkMiddleware } from '@clerk/express';
-// import bodyParser from 'body-parser';
-
-// dotenv.config();
-// await connectDB();
-// await connectcloudinary();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(clerkMiddleware());
-// app.post('/webhooks',
-//   bodyParser.raw({ type: 'application/json' }),clerkWebhooks
-// )
-// app.use(express.json());
-// app.get('/', (req, res) => res.send('API Working for insideJobs'));
-// app.use('/api/company', companyRouters);
-// app.use('/api/jobs', jobRoutes);
-// app.use('/api/users', userRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
